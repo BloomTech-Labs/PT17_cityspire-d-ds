59e841e5325d742f9c8e5c01adca1cd29660056d
Erik Seguinte <erikseguinte@me.com>
2021-02-17 22:30:15 -0800

    --amend

--- app/db.py
+++ app/db.py
@@ -19,7 +19,6 @@ database = databases.Database(database_url)
 router = APIRouter()
 
 
-
 @router.get("/info")
 async def get_url():
     """Verify we can connect to the database,
@@ -34,41 +33,40 @@ async def get_url():
     return {"database_url": url_without_password}
 
 
-
-async def select(columns:Union[Iterable[Field_], Field_], city):
-    data = Table('data')
-    if type(columns) == str or type(columns)==Field:
+async def select(columns: Union[Iterable[Field_], Field_], city):
+    data = Table("data")
+    if type(columns) == str or type(columns) == Field:
         q = Query.from_(data).select(columns)
     else:
         cols = [data[x] for x in columns]
         q = Query.from_(data).select(*cols)
 
-    q = (q
-            .where(data.City == city.city)
-            .where(data.State == city.state)
-        )
+    q = q.where(data.City == city.city).where(data.State == city.state)
 
     value = await database.fetch_one(str(q))
     return value
 
+
 async def select_all(city):
-    data = Table('data')
+    data = Table("data")
     columns = (
         # 'lat', 'lon'
-        data['lat'].as_('latitude'),
-        data['lon'].as_('longitude'),
-        data['Crime Rating'].as_('crime'),
-        data['Rent'].as_('rental_price'),
-        data['Air Quality Index'].as_('air_quality_index'),
-        data['Population'].as_('population'),
-        data['Nearest'].as_('nearest_string'),
-        data['Good Days'].as_('good_days'),
-        data["Crime Rate per 1000"].as_('crime_rate_ppt')
+        data["lat"].as_("latitude"),
+        data["lon"].as_("longitude"),
+        data["Crime Rating"].as_("crime"),
+        data["Rent"].as_("rental_price"),
+        data["Air Quality Index"].as_("air_quality_index"),
+        data["Population"].as_("population"),
+        data["Nearest"].as_("nearest_string"),
+        data["Good Days"].as_("good_days"),
+        data["Crime Rate per 1000"].as_("crime_rate_ppt"),
     )
-    q = (Query.from_(data).select(*columns)
-            .where(data.City == city.city)
-            .where(data.State == city.state)
-        )
+    q = (
+        Query.from_(data)
+        .select(*columns)
+        .where(data.City == city.city)
+        .where(data.State == city.state)
+    )
 
     value = await database.fetch_one(str(q))
     return value
