2e74f3b2336c7836b2531c1cef56d9d1be8942cb
jordantcarlisle <jordantcarlisle@gmail.com>
2021-03-31 23:10:50 -0500

    added new exgernal route to main.py and added external.py api endpoint for job opportunities

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-from app import db, ml, viz
+from app import db, ml, viz, external
 
 description = """
 MISSION: Be a one-stop resource for users to receive the most accurate city information.
@@ -26,6 +26,8 @@ app = FastAPI(
 app.include_router(db.router, tags=["Database"])
 app.include_router(ml.router, tags=["Machine Learning"])
 app.include_router(viz.router, tags=["Visualization"])
+app.include_router(external.router, tags=["External"])
+
 
 app.add_middleware(
     CORSMiddleware,
